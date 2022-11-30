de98ed3d28fb7c43874896ca14c2f1e2b674341c
ArtemLevin <python_study_2022@mail.ru>
2022-12-01 00:48:04 +0300

    Creating Arrays

---
+++ CreatingArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray (int[] collection)
+
+{
+    int count = collection.Length;
+    int index = 0;
+    while (index < count)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+
+}
+
+void PrintArray (int[] col)
+{
+    int position = col.Length;
+    int pos = 0;
+    while (pos < position)
+    {
+        Console.Write (" " + col[pos]);
+        pos++;
+    }
+}
+
+int[] array = new int [10];
+FillArray(array);
+PrintArray(array);
